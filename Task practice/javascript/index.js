07e18d3f4d3a8baad8f28fd7256150d6f705cac6
Omolewaaye <105572921+Omolewaaye@users.noreply.github.com>
2022-10-05 02:17:28 +0100

    part 1.0

---
+++ Task practice/javascript/index.js
@@ -0,0 +1,22 @@
+let firstchildage = 24;
+var secondchildage = 20;
+const totalageofchildren = firstchildage + secondchildage;
+
+alert (totalageofchildren)
+
+
+function anyname() {
+    const name =" Damilola";
+    let age = 20;
+    let anotherage = 30;
+    let totalage =age + anotherage;
+
+}
+
+document.getElementById("ladies").innerHTML= "the ladies are here" 
+
+
+//write a conditional to show which number is larger between two numbers/*
+let (greater num){
+    case  30: console.log("i am ")
+}
---
+++ Task practice/javascript/personalpractice.js
@@ -0,0 +1,4 @@
+alert ("mary")
+alert ("what a beautiful world")
+let firstchildage = 54;
+this is my MDD branch
